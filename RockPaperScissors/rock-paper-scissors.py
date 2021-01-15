81ead9cc8b5727d6a69476bca1df815385525b8d
anickel101 <anson.nickel@gmail.com>
2021-01-15 10:47:33 -0500

    RPS outline

---
+++ RockPaperScissors/rock-paper-scissors.py
@@ -0,0 +1,14 @@
+import random
+
+def game_prompt():
+    print ("Hello! Welcome to Rocks, Paper, Scissors...Shoot!")
+
+game_prompt()
+
+def get_move(player):
+    # function takes in a player and outputs print rocks/paper/scissors text, and return an int
+
+def compare_move(m1, m2):
+    # compares the two moves, returns a win/loss
+
+def score()
